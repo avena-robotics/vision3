0afd62c14c92db67b5b03ba78512a6260a650e14
Maciej Szulc <93993291+maciej-szulc-robotics@users.noreply.github.com>
2021-12-02 17:01:58 +0100

    Create get_basler_all_images.py

---
+++ baslercam/basler_client_functions/get_basler_all_images.py
@@ -0,0 +1,21 @@
+from cv_bridge import CvBridge
+from custom_interfaces.srv import GetAllImages
+import numpy as np
+from basler_client_functions.call_empty_service_and_get_data import call_empty_service_and_get_data
+
+
+def get_basler_all_images() -> tuple:
+    """
+    This function is responsible to call ROS2 service which is responsible for 
+    getting two mono and color images from cameras.
+
+    :return tuple(np.ndarray, np.ndarray, np.ndarray) with left mono, color and right mono (in that order) or (np.array([]), np.array([]), np.array([])) if it fails
+    """
+    service_result: GetAllImages.Response = call_empty_service_and_get_data('get_basler_all_images', GetAllImages)
+    if service_result:
+        bridge = CvBridge()
+        left_mono = bridge.imgmsg_to_cv2(service_result.left_mono)                
+        right_mono = bridge.imgmsg_to_cv2(service_result.right_mono) 
+        color = bridge.imgmsg_to_cv2(service_result.color)  
+        return left_mono, color, right_mono
+    return np.array([]), np.array([]), np.array([])
