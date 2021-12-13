d6a8fe43ce2092551c710d50911de91aa31d2929
Bartlomiej Styczen <bartlomiej.styczen@pomagier.info>
2021-12-13 16:15:44 +0100

    Cleaning code to remove usage of Open3D structures only when it is needed

--- intelcam/pipeline/save_ortho_view.py
+++ intelcam/pipeline/save_ortho_view.py
@@ -9,6 +9,7 @@ import os
 import copy
 import cv2
 from numba import jit
+import matplotlib.pyplot as plt
 
 
 def open_intel_camera(config_filename):
@@ -34,7 +35,7 @@ def open_intel_camera(config_filename):
 
     # Start acquiring images
     rscam.start_capture(start_record=False)
-    print('Waiting 2 seconds for camera to start...\n')
+    print('Waiting a few seconds for camera to start...\n')
     time.sleep(1)
 
     # Return results
@@ -42,12 +43,24 @@ def open_intel_camera(config_filename):
 
 
 def get_intel_all_images(camera_handle) -> Tuple[np.ndarray, np.ndarray]:
+    """Reads last from Intel camera.
+    
+    This function is responsible for reading last color and depth 
+    frame in which depth is aligned to color frame. Color is RGB 
+    where each channel is in range [0; 255] and depth is distance
+    in millimeters.
+
+    Args:
+        camera_handle: handle to Intel camera (it is assumed that camera is opened)
+
+    Returns:
+        Tuple with 2 elements:
+            - color array: each channel is in [0; 255] range (np.ndarray),
+            - depth array: distance in millimeters (np.ndarray)
     """
-    TODO
-    """
-    rgbd_frame = camera_handle.capture_frame(align_depth_to_color=True)
-    color = rgbd_frame.color
-    depth = rgbd_frame.depth
+    rgbd_frame = camera_handle.capture_frame(align_depth_to_color=True, wait=True)
+    color = np.array(rgbd_frame.color.to_legacy())
+    depth = np.array(rgbd_frame.depth.to_legacy())
     return color, depth
 
 
@@ -59,16 +72,16 @@ def close_intel_camera(camera_handle):
 
 
 @jit(parallel=True, fastmath=True)
-def calculate_rgbd_orthophoto(points: np.ndarray, colors: np.ndarray, voxel_size: float = 0.001) -> Tuple[np.ndarray, np.ndarray]:
+def calculate_rgbd_orthophoto(points: np.ndarray, colors: np.ndarray, voxel_size: float = 1) -> Tuple[np.ndarray, np.ndarray]:
     """Calculates orthographic view from colored point cloud
     
     This function is responsible for calculating orthographic view using colored point cloud.
     It first voxelizes provided 3D points in XY plane with specified voxel size.  
 
     Args:
-        points: array with 3D coordinates of point cloud (each coordinate is assumed to be in meters) (np.ndarray),
+        points: array with 3D coordinates of point cloud (each coordinate is assumed to be in millimeter) (np.ndarray),
         colors: array with RGB pixel (each channel in pixel is assumed to be in range [0, 1] (np.ndarray),
-        voxel_size: size of each voxel in XY plane in meters (float); default: 0.001, 
+        voxel_size: size of each voxel in XY plane in millimeter (float); default: 1, 
 
     Returns:
         Tuple with 2 elements:
@@ -78,12 +91,14 @@ def calculate_rgbd_orthophoto(points: np.ndarray, colors: np.ndarray, voxel_size
     # Convert [0; 1] RGB to [0; 255] and cast to uint8
     colors = (colors * 255).astype(np.uint8)
 
+    # Convert type to int32
+    points = points.astype(np.int32)
+    # points = (points * 1000).astype(np.int32)
+    # voxel_size = int(voxel_size * 1000)
+    
     # Voxelize in XY plane
     points[:, 0] = (points[:, 0] - points[:, 0].min()) / voxel_size
     points[:, 1] = (points[:, 1] - points[:, 1].min()) / voxel_size
-    
-    # Convert meters to millimeters in Z axis
-    points[:, 2] = points[:, 2] * 1000
     
     # Cast to UINT16 type
     points = points.astype(np.uint16)
@@ -134,25 +149,31 @@ def calculate_rgbd_orthophoto(points: np.ndarray, colors: np.ndarray, voxel_size
 
 # Filters
 def trunc_depth(depth_frame, d):
+    """Truncate depth image.
+    
+    This function is responsible for truncating (zero out)
+    pixels which values are greater then specified minimal distance.
+
+    Args:
+        depth_frame: image with depth values (in millimeters),
+        d: maximum distance allowed (in millimeters)
+
+    Returns:
+        depth image with values greater than d set to 0
     """
-    TODO
-    """
-    arr_img = np.asarray(depth_frame.to_legacy())
+    arr_img = depth_frame.copy()
     arr_img[arr_img >= d] = 0
-    tensor = o3d.core.Tensor(arr_img)
-    image = o3d.t.geometry.Image(tensor)
-    return image
+    return arr_img
 
 
 def bilateral_filter(depth_frame, d, sigma_color, sigma_space):
     """
     TODO
     """
-    arr_img = np.asarray(depth_frame.to_legacy()).astype(np.float32)
-    arr_filtered = cv2.bilateralFilter(arr_img, d, sigma_color, sigma_space)
-    tensor = o3d.core.Tensor(arr_filtered.astype(np.uint16))
-    image = o3d.t.geometry.Image(tensor)
-    return image
+    arr_img = depth_frame.astype(np.float32)
+    arr_img = cv2.bilateralFilter(arr_img, d, sigma_color, sigma_space)
+    arr_img = arr_img.astype(np.uint16)
+    return arr_img
 
 
 def statistical_outlier_removal(point_cloud, nb_neighbors, std_ratio):
@@ -174,12 +195,29 @@ def median_blur(image: np.ndarray, kernel_size: int = 5) -> np.ndarray:
     return depth_array_median
 
 
+def create_point_cloud(color: np.ndarray, depth: np.ndarray, camera_info: dict):
+    """Convert RGB and depth image to Open3D point cloud using camera intrinsic
+    """
+    rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(color=o3d.geometry.Image(color),
+                                                                    depth=o3d.geometry.Image(depth),
+                                                                    depth_trunc=10000, # depth is filtered before in trunc_depth
+                                                                    depth_scale=1,
+                                                                    convert_rgb_to_intensity=False)
+    intrinsic = o3d.camera.PinholeCameraIntrinsic(width=camera_info["width"],
+                                                  height=camera_info["height"],
+                                                  fx=camera_info["fx"], fy=camera_info["fy"],
+                                                  cx=camera_info["cx"], cy=camera_info["cy"])
+    pcld = o3d.geometry.PointCloud.create_from_rgbd_image(image=rgbd_image,
+                                                          intrinsic=intrinsic)
+    return pcld
+
+
 if __name__ == '__main__':
     #########################################################
     # User modifies path for dataset
     BASE_DIR = '/home/avena/datasets/intel/dataset001'
     intel_configuration_file = '../config/cam9_config.json'
-    voxel_size = 0.001
+    voxel_size = 1 # millimeters
     median_filter_kernel = 5
     #########################################################
 
@@ -195,34 +233,26 @@ if __name__ == '__main__':
         while pressed_key != 'q':
             print('Reading color and depth images')
             color, depth = get_intel_all_images(intel_camera_handle)
-            if color.columns == 0 or depth.columns == 0:
+            cv2.imwrite('/home/avena/Pictures/color.png', cv2.cvtColor(color, cv2.COLOR_RGB2BGR))
+            if color.size == 0 or depth.size == 0:
                 print('Failed to get images. Try to reopen camera...')
                 close_intel_camera(intel_camera_handle)
                 intel_camera_handle, camera_info = open_intel_camera(intel_configuration_file)
                 continue
             
             print('Filtering depth in Z axis')
-            depth = trunc_depth(depth, 2000)
+            depth = trunc_depth(depth, 1500)
 
             # Create point cloud using Open3D
             print('Creating point cloud from RGBD image')
-            rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(color=color.to_legacy(),
-                                                                        depth=depth.to_legacy(),
-                                                                        depth_trunc=1.2,
-                                                                        convert_rgb_to_intensity=False)
-            intrinsic = o3d.camera.PinholeCameraIntrinsic(width=camera_info["width"],
-                                                    height=camera_info["height"],
-                                                    fx=camera_info["fx"], fy=camera_info["fy"],
-                                                    cx=camera_info["cx"], cy=camera_info["cy"])
-            pcld = o3d.geometry.PointCloud.create_from_rgbd_image(image=rgbd_image,
-                                                            intrinsic=intrinsic)
+            pcld = create_point_cloud(color, depth, camera_info)
 
-            print('Calculating orthographic view')
             # Get copy of points and colors
-            points = np.asarray(copy.deepcopy(pcld.points))
-            colors = np.asarray(copy.deepcopy(pcld.colors))
-
+            points = np.asarray(pcld.points)
+            colors = np.asarray(pcld.colors)
+            
             # Calculate orthographic view
+            print('Calculating orthographic view')
             rgb_array, depth_array = calculate_rgbd_orthophoto(points, colors, voxel_size)
 
             # print('Filtering')
@@ -230,6 +260,12 @@ if __name__ == '__main__':
             # # Median filter
             # rgb_array = median_blur(rgb_array, median_filter_kernel)
             # depth_array = median_blur(depth_array, median_filter_kernel)
+
+            # plt.imshow(rgb_array)
+            # plt.show()
+
+            # plt.imshow(depth_array)
+            # plt.show()
 
             # Save images
             ts_now = time.time_ns()
